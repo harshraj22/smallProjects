00e8a56a352cf0a83cc7b66e6ee9ac172c89b33f
harshraj22 <harshraj22aug@gmail.com>
2020-02-29 19:21:11 +0530

    updated methods to update teachers list

--- teachers.py
+++ teachers.py
@@ -6,21 +6,23 @@ class Teacher:
             raise Exception('No such teacher exists')
         self.is_teacher = True
     
-    def exists(self, name):
+    @staticmethod
+    def exists(name):
         teachers_list = DatabaseHandler().get_teachers_list()
         name = name.strip().lower()
         if name in teachers_list.keys():
             return True
         return False
 
-    def add_teacher(self, **kwargs):
+    @staticmethod
+    def add_teacher(**kwargs):
         # check for name and pass
-        if not 'name' in kwargs.keys() or not 'pass' in kwargs.keys():
+        if not 'name' in kwargs.keys() or not 'password' in kwargs.keys():
+            print(kwargs)
             raise Exception('Not enough data to create such Teacher')
-        elif self.exists(kwargs['name']):
+        elif Teacher.exists(kwargs['name']):
             raise Exception('Teacher already exists')
         teachers_list = DatabaseHandler().get_teachers_list()
-        teachers_list[kwargs['name']] = kwargs['pass']
+        teachers_list[kwargs['name']] = kwargs['password']
         DatabaseHandler().update_teachers_list(teachers_list)
-        
         
