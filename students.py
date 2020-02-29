447340010f3b8adbf37d08ef16f209b82a5fd740
harshraj22 <harshraj22aug@gmail.com>
2020-02-29 19:05:18 +0530

    added exist and add method in students class

--- students.py
+++ students.py
@@ -10,3 +10,20 @@ class Student:
         if name in students_list.keys():
             return True
         return False
+
+    def exists(self, name):
+        students_list = DatabaseHandler().get_students_list()
+        name = name.strip().lower()
+        if name in students_list.keys():
+            return True
+        return False
+
+    def add_student(self, **kwargs):
+        # check for name and pass
+        if not 'name' in kwargs.keys() or not 'pass' in kwargs.keys():
+            raise Exception('Not enough data to create such Teacher')
+        elif self.exists(kwargs['name']):
+            raise Exception('Teacher already exists')
+        students_list = DatabaseHandler().get_students_list()
+        students_list[kwargs['name']] = kwargs['pass']
+        DatabaseHandler().update_students_list(students_list)
