394bd209a50b69c22c366799b8ee887f6e6f9f04
harshraj22 <harshraj22aug@gmail.com>
2020-02-29 18:55:43 +0530

    added method to instantiate teacher class

--- teachers.py
+++ teachers.py
@@ -1,8 +1,10 @@
 from database_handler import DatabaseHandler
 
 class Teacher:
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        if not self.exists(name):
+            raise Exception('No such teacher exists')
+        self.is_teacher = True
     
     def exists(self, name):
         teachers_list = DatabaseHandler().get_teachers_list()
